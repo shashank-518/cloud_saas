c5083ef6994d41807b7751685a586ef9bbf57923
Shashank S <shashank5418shashu@gmail.com>
2025-06-27 12:44:17 +0530

    done with image upload

--- my-app/src/app/api/image-upload/route.ts
+++ my-app/src/app/api/image-upload/route.ts
@@ -3,7 +3,7 @@ import { auth } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
