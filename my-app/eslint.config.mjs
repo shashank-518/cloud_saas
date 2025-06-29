a545bb70bab09692578ff5a762d2ce387be82cd0
Shashank S <shashank5418shashu@gmail.com>
2025-06-29 13:04:56 +0530

    changeesss

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -21,7 +21,8 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unnecessary-type-constraint": "off",
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-unused-expressions": "off", // ✅ disable expression rule here
+      "@typescript-eslint/no-unused-expressions": "off", 
+      "@typescript-eslint/no-this-alias": "off",
     },
   },
 
